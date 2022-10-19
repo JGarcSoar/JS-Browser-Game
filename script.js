674e7b52221b176d40b519cd5508d8ed70f7aa29
JGarcSoar <garciasoares.joe@gmail.com>
2022-10-18 21:30:43 -0700

    added sounds and timer

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
+//constants and variables
 const buttonStart = document.getElementById("start-btn");
-const buttonContinue = document.getElementById("continue-btn");
-const buttonGiveUp = document.getElementById("giveup-btn");
+const buttonNext = document.getElementById("next-btn");
+const buttonQuit = document.getElementById("quit-btn");
 const buttonSkip = document.getElementById("skip-btn");
 const questionContainerDiv = document.getElementById("question-container");
 const questionElement = document.getElementById("question");
 const answerButton = document.getElementById("answer-btn");
+const scoreKeeper = document.getElementById("score");
+const letsPlayAudio = document.getElementById("lets-play");
+const easyAudio = document.getElementById("easy");
+const wrongAnswerAudio = document.getElementById("wrong-answer");
+const rightAnswerAudio = document.getElementById("right-answer");
+const countDownTimer = document.getElementById("timer");
 
 let questionShuffle, currentQuestion;
 
+questionElement.innerText = "";
+
+//event listeners
 buttonStart.addEventListener("click", gameStart);
-buttonContinue.addEventListener("click", () => {
+buttonNext.addEventListener("click", () => {
   currentQuestion++;
   nextQuestion();
 });
@@ -18,7 +28,9 @@ buttonSkip.addEventListener("click", () => {
   nextQuestion();
   buttonSkip.style.display = "none";
 });
+buttonQuit.addEventListener("click", quit);
 
+//functions
 function gameStart() {
   console.log("started");
   buttonSkip.classList.remove("hide");
@@ -26,13 +38,15 @@ function gameStart() {
   currentQuestion = 0;
   questionContainerDiv.classList.remove("hide");
   nextQuestion();
+  startTimerSound();
 }
 
 function nextQuestion() {
   stateReset();
   questionShow(questionShuffle[currentQuestion]);
   buttonStart.classList.add("hide");
-  buttonGiveUp.classList.remove("hide");
+  buttonQuit.classList.remove("hide");
+  startTimerSound();
 }
 
 function questionShow(question) {
@@ -49,7 +63,18 @@ function questionShow(question) {
   });
 }
 
-function giveUp() {}
+function quit() {
+  clearStatus(document.body);
+  buttonStart.classList.remove("hide");
+  buttonNext.classList.add("hide");
+  buttonSkip.classList.add("hide");
+  buttonQuit.classList.add("hide");
+  questionElement.innerText = "";
+  stopTimerSound();
+  while (answerButton.firstChild) {
+    answerButton.removeChild(answerButton.firstChild);
+  }
+}
 
 function answerSelect(e) {
   let selectedBtn = e.target;
@@ -59,7 +84,7 @@ function answerSelect(e) {
     statusClassSet(button, button.dataset.correct);
   });
   if (questionShuffle.length > currentQuestion + 1) {
-    buttonContinue.classList.remove("hide");
+    buttonNext.classList.remove("hide");
   } else {
     buttonStart.innerText = "Restart";
     buttonStart.classList.remove("hide");
@@ -70,8 +95,11 @@ function statusClassSet(element, correct) {
   clearStatus(element);
   if (correct) {
     element.classList.add("correct");
+    stopTimerSound();
+    rightAnswerAudio.play();
   } else {
     element.classList.add("incorrect");
+    gameOver();
   }
 }
 
@@ -82,12 +110,58 @@ function clearStatus(element) {
 
 function stateReset() {
   clearStatus(document.body);
-  buttonContinue.classList.add("hide");
+  buttonNext.classList.add("hide");
   while (answerButton.firstChild) {
     answerButton.removeChild(answerButton.firstChild);
   }
 }
 
+function timer() {
+  currentTime = new Date().getTime();
+  intervalId = setInterval(() => {
+    const interval = Math.floor(
+      (40000 + currentTime - new Date().getTime()) / 1000
+    );
+    countDownTimer.textContent = interval;
+    if (interval === 0) {
+      clearInterval(intervalId);
+      gameOver();
+    }
+
+    return interval;
+  }, 100);
+}
+
+function startTimerSound() {
+  timer();
+  letsPlayAudio.play();
+  letsPlayAudio.volume = 0.3;
+  timeoutId = setTimeout(() => {
+    easyAudio.play();
+    easyAudio.volume = 0.3;
+  }, 4000);
+}
+
+function stopTimerSound() {
+  clearTimeout(timeoutId);
+  clearInterval(intervalId);
+  letsPlayAudio.pause();
+  letsPlayAudio.currentTime = 0;
+  easyAudio.pause();
+  easyAudio.currentTime = 0;
+  wrongAnswerAudio.pause();
+  wrongAnswerAudio.currentTime = 0;
+  rightAnswerAudio.pause();
+  rightAnswerAudio.currentTime = 0;
+}
+
+function gameOver() {
+  stopTimerSound();
+  wrongAnswerAudio.play();
+  wrongAnswerAudio.volume = 0.3;
+}
+
+// question list
 const questions = [
   {
     question: "What is 2 / 2?",
@@ -121,4 +195,4 @@ const questions = [
   },
 ];
 
-// needs to add functionality to the giveup button, needs to add score meter and needs to add more questions
+// add more questions, fix the sound issues and add gameover rules
